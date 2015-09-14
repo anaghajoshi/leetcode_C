8553ba8adbcd89012377f9c446e21204f261ad78
Anagha Joshi <anagha.elex@gmail.com>
2015-09-14 02:11:51 -0400

    Add isPowerOfTwo

--- is_power_of_two.c
+++ is_power_of_two.c
@@ -1,3 +1,17 @@
+/*
+Given an integer, write a function to determine if it is a power of two.
+
+Solution:
+The binary representation of a power-of-two 2y is a 1 followed only by 0s.
+In such a case, x − 1 generates a binary number where the 1s turn into 0s and the former 0s turn into 1s.
+For example, 8 = 1000b and 8 − 1 = 7 = 0111b.
+
+If x and x − 1 are binary ANDed then the result is only 0 if x is a power of two (line 3).
+
+Restrictions	• the algorithm returns true for x = 0 and x = 1, i.e. they are considered to be a power of two
+
+Thus add if condition to take care of that limitation.
+*/
 bool isPowerOfTwo(int n) {
     if (n <=0){
         return false;
